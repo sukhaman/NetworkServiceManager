93cfe033f6fa40d1e44263c26f1c57e31fee4ca4
Sukhaman Singh <sukhamandev@gmail.com>
2024-01-14 15:16:05 -0500

    framework added to hide the private from public

--- Package.swift
+++ Package.swift
@@ -12,12 +12,7 @@ let package = Package(
             targets: ["NetworkServiceManager"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "NetworkServiceManager"),
-        .testTarget(
-            name: "NetworkServiceManagerTests",
-            dependencies: ["NetworkServiceManager"]),
+    
+        .binaryTarget(name: "NetworkServiceManager", path: "./Sources/NetworkServiceManager.xcframework")
     ]
 )
---
+++ Sources/NetworkServiceManager.xcframework/ios-arm64/NetworkServiceManager.framework/Headers/NetworkServiceManager.h
@@ -0,0 +1,18 @@
+//
+//  NetworkServiceManager.h
+//  NetworkServiceManager
+//
+//  Created by Sukhaman Singh on 1/14/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkServiceManager.
+FOUNDATION_EXPORT double NetworkServiceManagerVersionNumber;
+
+//! Project version string for NetworkServiceManager.
+FOUNDATION_EXPORT const unsigned char NetworkServiceManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkServiceManager/PublicHeader.h>
+
+
